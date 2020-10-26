db27e145858446dd1b326e537bc26e187916031c
vedransehovic <vedran.sehovic@gmail.com>
2020-10-26 18:30:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+function fetchImages() {
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json));
+  }
+
+function renderImages(imgURLs) {
+    //debugger
+    imgURLs["message"].forEach(displayImgs)
+
+    function displayImgs(item) {
+        document.getElementById("dog-image-container").innerHTML += `<img src="${item}">`
+    }
+}
+
+function fetchBreeds() {
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json));
+}
+
+function renderBreeds(breeds) {
+    //console.log(breeds)
+    breeds["message"].forEach(printName)
+
+    function printName(item) {
+        console.log(item)
+    }
+}
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+    fetchBreeds()
+  })
