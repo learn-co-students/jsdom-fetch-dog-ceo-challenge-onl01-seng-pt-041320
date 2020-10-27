0968de17d597cc9e270d93f1a1d93a4118a85961
OrkunSA <c.orkun.s@gmail.com>
2020-10-26 21:13:42 -0500

    adding solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages() {
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        results.message.forEach(image => renderImages(image))
+    });
+}
+
+function renderImages(url) {
+    let container = document.getElementById('dog-image-container')
+    let newImage = document.createElement('img');
+    newImage.src = url;
+    container.appendChild(newImage);
+}   
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+  })
