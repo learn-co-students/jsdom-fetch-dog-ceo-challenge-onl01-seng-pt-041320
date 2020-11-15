af67a6783811c16bdb55f4d9ee778d96262777a5
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-11-14 19:26:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,107 +1,90 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+const breedList = document.getElementById("dog-breeds")
+var filterList = []
 
-const dogList = document.querySelector("#dog-breeds")
-const dropdown = document.querySelector("#breed-dropdown")
+document.addEventListener('DOMContentLoaded', function() {
+  fetchDogs()
+  pictureDogs()
+  filterDogs()
+})
 
-// Approach 1: Stateful
-// store all the breeds in an array
-// filter the array
-// (also, clear the list)
-// use that list to display
+function displayDogs(images) {
+  const pics = images.message
+  const container = document.getElementById("dog-image-container")
 
-// Approach 2: DOM Source Of Truth
-// iterate over all the lis
-// if they don't start with the letter, hide them with CSS
+  pics.forEach(image => 
+    {
+      var img = document.createElement('img');
+      img.src = image
+      container.appendChild(img);
+    });
+};
 
-// State
-let breedList = []
+function listDogs(breeds) {
+  const list = breeds.message
+  Object.keys(list).forEach(dog => {
+    var li = document.createElement('li');
+    li.innerText = dog
+    breedList.appendChild(li);
+    filterList.push(dog)
+  });
+};
 
-// added the event listener to the dropdown
-dropdown.addEventListener("change", event => {
-  // got the selected value
-  const letter = event.target.value
-
-  // filter the array
-  const filteredBreeds = breedList.filter(function(breed) {
-    return breed.startsWith(letter)
-  })
+function filterDogs() {
+  document.getElementById("breed-dropdown").addEventListener('change', event => {
+    event.preventDefault
+    filter = []
+    const value = document.getElementById("breed-dropdown").value
+    switch(value) {
+      case "a":
+        filterList.forEach(dog => {
+          if (dog[0] == "a") {
+            filter.push(dog)
+          }
+        })
+        break;
+      case "b":
+        filterList.forEach(dog => {
+          if (dog[0] == "b") {
+            filter.push(dog)
+          }
+        })
+        break;
+      case "c":
+        filterList.forEach(dog => {
+          if (dog[0] == "c") {
+            filter.push(dog)
+          }
+        })
+        break;
+      case "d":
+        filterList.forEach(dog => {
+          if (dog[0] == "d") {
+            filter.push(dog)
+          }
+        })
+        break;
+    }
+    breedList.innerHTML = ""
+    filter.forEach(dog => {
   
-  // (also, clear the list)
-  dogList.innerHTML = ''
+      var li = document.createElement('li');
+      li.innerText = dog
+      breedList.appendChild(li);
+    });
+  });
 
-  // use that list to display
-  filteredBreeds.forEach(function(breed) {
-    renderBreed(breed)
-  })
-
-  console.log(filteredBreeds)
-
-  // iterate over all the list items
-  // dogList.querySelectorAll("li").forEach(li => {
-  //   // check if it starts with the letter
-  //   if (!li.textContent.startsWith(letter)) {
-  //     // hide it!
-  //     li.style.display = "none"
-  //   } else {
-  //     li.style.display = ""
-  //   }
-  // })
-})
-
-// find the closest parent
-dogList.addEventListener("click", event => {
-  // use a condition to figure out what element was clicked
-  if (event.target.matches("li")) {
-    // run our DOM logic
-    event.target.style.color = "red"
-  }
-})
-
-function renderImage(imageUrl) {
-  const img = document.createElement("img")
-  img.src = imageUrl
-  const imageContainer = document.querySelector("#dog-image-container")
-  imageContainer.append(img)
 }
 
-function renderAllImages(data) {
-  // when we have the data
-  const images = data.message
-    
-  // append to the DOM
-  images.forEach(imageUrl => {
-    renderImage(imageUrl)
-  })
-}
+function pictureDogs() {
+  return fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => displayDogs(json))
+};
 
-function loadImages() {
-  // when the page loads
-  // make a GET request
-  fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(response => response.json())
-    .then(renderAllImages)
-}
-
-
-function renderBreed(breed) {
-  const li = document.createElement("li")
-  li.textContent = breed
-  dogList.append(li)
-}
-
-function loadBreeds() {
-  fetch("https://dog.ceo/api/breeds/list/all")
-    .then(response => response.json())
-    .then(data => {
-      // Object.keys
-      // for..in
-      breedList = Object.keys(data.message)
-
-      breedList.forEach(breed => {
-        renderBreed(breed)
-      })
-    })
-}
-
-loadBreeds()
-loadImages()
+function fetchDogs() {
+  return fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(json => listDogs(json))
+};
